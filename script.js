3521dfac681885a9dc80e3e97eaf28cbce2cc775
trent <122744132+trentbenson08@users.noreply.github.com>
2023-02-24 20:52:46 -0600

    Make data.json and button input display in console

--- script.js
+++ script.js
@@ -1,5 +1,35 @@
+
 function main(){
-    console.log("hello world");
+
+    // fetches json.data and puts the data through loadCards()
+    fetch('data.json').then((response) => {
+        return response.json();
+    }).then(json => {loadCards(json)}).catch((error) => {
+        console.error('Something went wrong with retrieving flashcard data.');
+        console.error(error);
+    });
+    
+    // event listner for onscreen buttons puts event info through handleInput()
+    const buttons = document.querySelectorAll('button');
+    buttons.forEach((btn) => {
+        btn.addEventListener('click', (e) => {handleInput(e)});
+    })
+
+
+
 }
+
+function loadCards(cards){
+    const infoDisplay = document.querySelector('#set-info>p');
+    console.log(infoDisplay, cards.default.length)
+    infoDisplay.textContent = cards.default.length;
+}
+
+function handleInput(event){
+    let input = event.target.getAttribute('data-val')
+    console.log(input)
+}
+
+
 
 main();
